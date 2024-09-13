d500afb2f557686226fd330ed107408ba46595ef
quanquan <3252983692@qq.com>
2024-09-13 17:40:10 +0800

    20240913

--- hashTable.cpp
+++ hashTable.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
+#include <algorithm>
 using string = std::string;
 template <typename T>
 using vector = std::vector<T>;
@@ -124,5 +125,89 @@ public:
             }
         }
         return count;
+    }
+};
+
+// 三数之和
+class Solution
+{
+public:
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0) // 三元组里面最小的数都大于0了不行
+                return result;
+            // 三元素里面的值可以相等，找前面有没有相等的元素，前面相等说明前面和自己相等的这个首元素已经做好了找三元组的过程，这时候跳过没有问题
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (right > left) // 如果left right重复逻辑放在这里面可能直接导致right<=left从而漏掉{000}
+            {
+                if (nums[i] + nums[left] + nums[right] > 0)
+                    right--;
+                else if (nums[i] + nums[left] + nums[right] < 0)
+                    left++;
+                else
+                    result.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                while (right > left && nums[right] == nums[right - 1])
+                    right--;
+                while (right > left && nums[left] == nums[left + 1])
+                    left++;
+                // 收获一个结果之后，双指针同时收缩
+                left++;
+                right--;
+            }
+        }
+        return result;
+    }
+};
+
+// 四数之和
+
+class Solution
+{
+public:
+    vector<vector<int>> fourSum(vector<int> &nums, int target)
+    {
+        vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        for (int k = 0; k < nums.size(); k++)
+        { // 剪枝
+            if (nums[k] > target && nums[k] > 0 && target > 0)
+            {
+                break;
+            }
+            if (k > 0 && nums[k] == nums[k - 1])
+                continue;
+            for (int i = k + 1; i < nums.size(); i++)
+            {
+                if (i > k + 1 && nums[i] == nums[i - 1])
+                    continue;
+                int left = i + 1;
+                int right = nums.size() - 1;
+                while (right > left)
+                {
+                    if (nums[k] + nums[i] + nums[left] + nums[right] > target)
+                        right--;
+                    else if (nums[k] + nums[i] + nums[left] + nums[right] < target)
+                        left++;
+                    else
+                    {
+                        result.push_back(vector<int>{nums[k], nums[i], nums[left], nums[right]});
+                        while (right > left && nums[right] == nums[right - 1])
+                            right--;
+                        while (right > left && nums[left] == nums[left + 1])
+                            left++;
+                        right--;
+                        left++;
+                    }
+                }
+            }
+        }
+        return result;
     }
 };
