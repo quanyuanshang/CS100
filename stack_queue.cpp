438dbf513faa76835741a04eb464d793b4dea620
quanquan <3252983692@qq.com>
2024-09-23 18:10:11 +0800

    20240923

--- stack_queue.cpp
+++ stack_queue.cpp
@@ -2,13 +2,19 @@
 #include <queue>
 #include <string>
 #include <vector>
-template <typename T>
+#include <deque>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+/*template <typename T>
 using stack = std::stack<T>;
 template <typename T>
 using queue = std::queue<T>;
 using string = std::string;
 template <typename T>
 using vector = std::vector<T>;
+template <typename T>
+using deque = std::deque<T>;*/
 // 用栈实现队列
 class MyQueue
 {
@@ -176,5 +182,91 @@ public:
         int res = st.top();
         st.pop(); // 释放内存
         return res;
+    }
+};
+
+// 滑动窗口最大值
+class Solution
+{
+private:
+    class Myqueue
+    {
+    public:
+        deque<int> que;
+        void pop(int val)
+        {
+            if (!que.empty() && val == que.front())
+                que.pop_front();
+        }
+        void push(int val)
+        {
+            while (!que.empty() && val > que.back())
+                que.pop_back();
+            que.push_back(val);
+        }
+        int front()
+        {
+            return que.front();
+        }
+    };
+
+public:
+    vector<int>
+    maxSlidingWindow(vector<int> &nums, int k)
+    {
+        Myqueue que;
+        vector<int> result;
+        for (int i = 0; i < k; i++)
+        {
+            que.push(nums[i]);
+        }
+        result.push_back(que.front());
+        for (int i = k; i < nums.size(); i++)
+        {
+            que.pop(nums[i - k]); // 前一个窗口的首元素
+            que.push(nums[i]);    // 后一个窗口的首元素
+            result.push_back(que.front());
+        }
+        return result;
+    }
+};
+
+// 求前k个高频元素
+class Solution
+{
+public:
+    class mycomparison
+    {
+    public:
+        bool operator()(const pair<int, int> lhs, const pair<int, int> rhs)
+        {
+            return lhs.second > rhs.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            map[nums[i]]++;
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, mycomparison> pri_que;
+        for (unordered_map<int, int>::iterator it = map.begin(); it != map.end(); it++)
+        {
+            pri_que.push(*it);
+            if (pri_que.size() > k)
+            {
+                pri_que.pop();
+            }
+        } // 每次让出现频次小的数出去
+        // 倒序遍历输出
+        vector<int> result(k);
+        for (int i = k - 1; i >= 0; i--)
+        {
+            result[i] = pri_que.top().first;
+            pri_que.pop();
+        }
+
+        return result;
     }
 };
