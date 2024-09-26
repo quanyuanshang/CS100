a9c10fb74930d3433e54934e88071d26429532ea
quanquan <3252983692@qq.com>
2024-09-26 22:50:01 +0800

    20240925

---
+++ tree.cpp
@@ -0,0 +1,77 @@
+#include <stack>
+#include <queue>
+#include <string>
+#include <vector>
+#include <deque>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+// 前序遍历
+class Solution
+{
+public:
+    void traversal(TreeNode *cur, vector<int> &vec)
+    {
+        if (cur == NULL)
+            return;
+        vec.push_back(cur->val);
+        traversal(cur->left, vec);
+        traversal(cur->right, vec);
+    }
+    vector<int> preorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};
+// 中序遍历
+class Solution
+{
+public:
+    void traversal(TreeNode *cur, vector<int> &vec)
+    {
+        if (cur == NULL)
+            return;
+
+        traversal(cur->left, vec);
+        vec.push_back(cur->val);
+        traversal(cur->right, vec);
+    }
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};
+// 后序
+class Solution
+{
+public:
+    void traversal(TreeNode *cur, vector<int> &vec)
+    {
+        if (cur == NULL)
+            return;
+
+        traversal(cur->left, vec);
+
+        traversal(cur->right, vec);
+        vec.push_back(cur->val);
+    }
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        traversal(root, result);
+        return result;
+    }
+};
