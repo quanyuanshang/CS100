0aef2215cbeb818cb2a3b6641b08ff2e108267a1
quanquan <3252983692@qq.com>
2024-10-05 15:15:07 +0800

    2024/10/5

--- tree.cpp
+++ tree.cpp
@@ -5,6 +5,7 @@
 #include <deque>
 #include <unordered_map>
 #include <queue>
+#include <algorithm>
 using namespace std;
 struct TreeNode
 {
@@ -73,5 +74,126 @@ public:
         vector<int> result;
         traversal(root, result);
         return result;
+    }
+};
+// 非递归遍历
+class Solution
+{
+public:
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        stack<TreeNode *> st;
+        st.push(root);
+        TreeNode *node;
+        while (!st.empty())
+        {
+            node = st.top();
+            st.pop();
+            if (node != nullptr)
+            {
+                result.push_back(node->val); // 中
+            }
+            else
+            {
+                continue;
+            }
+            st.push(node->right); // 右
+            st.push(node->left);  // 左
+        }
+        return result;
+    }
+};
+class Solution // 后续遍历
+{
+public:
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        stack<TreeNode *> st;
+        st.push(root);
+        TreeNode *node;
+        while (!st.empty())
+        {
+            node = st.top();
+            st.pop();
+            if (node != nullptr)
+            {
+                result.push_back(node->val); // 中
+            }
+            else
+            {
+                continue;
+            }
+
+            st.push(node->left);  // 左
+            st.push(node->right); // 右
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+class Solution // 后续中序
+{
+public:
+    vector<int> inorderTraversal(TreeNode *root)
+    {
+        vector<int> result;
+        stack<TreeNode *> st;
+        TreeNode *cur = root;
+        while (cur != nullptr || !st.empty())
+        {
+            if (cur != nullptr)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+            else
+            {
+                cur = st.top();
+                st.pop();
+                result.push_back(cur->val);
+                cur = cur->right;
+            }
+        }
+        return result;
+    }
+};
+
+// 层序遍历
+class Solution
+{
+public:
+    int minDepth(TreeNode* root)
+    {
+        queue<TreeNode *> que;
+        TreeNode *node;
+        int layer=0;
+        int min_layer = INT_MAX;
+        if (root != NULL)
+            que.push(root);
+        while (!que.empty())
+        {
+            int size = que.size(); // 不记录的话容易和下一层的元素一起弹出来，这是控制弹出数目
+            vector<int> vec;
+            layer++;
+            while (size--)//在循环里que的size是不断变化的
+            {
+                node = que.front();
+                que.pop();
+                vec.push_back(node->val);
+                if (node->left)
+                    que.push(node->left);
+                if (node->right)
+                    que.push(node->right);
+                    if(!node->right &&!node->left){
+                        if(min_layer>=layer)
+                            min_layer = layer;
+                    }
+                    
+            }
+           
+        }
+        return min_layer==INT_MAX?0:min_layer;
     }
 };
