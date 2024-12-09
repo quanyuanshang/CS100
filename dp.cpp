395beb3d9497d0b42d1e53e72dadf8b96870e7b3
quanquan <3252983692@qq.com>
2024-12-09 15:43:13 +0800

    20241209

--- dp.cpp
+++ dp.cpp
@@ -29,3 +29,116 @@ public:
         return dp[n];
     }
 };
+// 爬楼梯
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+        vector<int> dp(n + 1);
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+};
+// 不同路径
+class Solution
+{
+public:
+    int uniquePaths(int m, int n)
+    {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++)
+        {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}; // 不同路径2
+class Solution
+{
+public:
+    int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid)
+    {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++)
+        {
+            dp[i][0] = 1;
+        }
+        for (int i = 0; i < n && obstacleGrid[0][i] == 0; i++)
+        {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                if (obstacleGrid[i][j] == 1)
+                {
+                    continue;
+                }
+
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+// 整数拆分
+class Solution
+{
+public:
+    int integerBreak(int n)
+    {
+        vector<int> dp(n + 1); // 一定要初始化，确保每个位置一开始都是有值的。
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++)
+        {
+            for (int j = 1; j < i; j++)
+            {
+                dp[i] = max(dp[i], max((i - j) * j, dp[i - j] * j));
+            }
+        }
+        return dp[n];
+    }
+};
+// 不同的二叉搜索树
+class Solution
+{
+public:
+    int numTrees(int n)
+    {
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+};
